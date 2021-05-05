d2606320da20c139fbda9f1a61f809ec194e5a09
PavelSuchkov <suchkovp@gmail.com>
2021-05-05 17:39:17 +0300

    31.1 Spinner was changed

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,9 +1,10 @@
 import preloader_spinner from "../../../assets/images/preloader_spinner.svg";
+import preloader_gears from "../../../assets/images/preloader_gears.svg";
 import React from "react";
 
 
 export const Preloader = () => {
     return <div>
-    <img src={preloader_spinner} style={ {width: 50, backgroundColor: "transparent"} }/>
+    <img src={preloader_gears} style={ {width: 100, backgroundColor: "transparent"} }/>
 </div>
 }
